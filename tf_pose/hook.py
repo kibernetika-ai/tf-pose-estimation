8faac0f70c18e3a0b2fe6cbac73244acb20bad29
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-09-09 14:39:08 +0400

    Allow set resolution

--- tf_pose/hook.py
+++ tf_pose/hook.py
@@ -9,16 +9,18 @@ e: estimator.TfPoseEstimator = None
 PARAMS = {
     'model': 'mobilenet_thin',
     'resize_out_ratio': 4.0,
+    'target_size': (432, 368)
 }
 
 
 def init_hook(**params):
     PARAMS.update(params)
     PARAMS['resize_out_ratio'] = float(PARAMS['resize_out_ratio'])
+    PARAMS['target_size'] = _parse_resolution(PARAMS['target_size'])
     global e
 
     e = estimator.TfPoseEstimator(
-        networks.get_graph_path(PARAMS['model']), target_size=(432, 368)
+        networks.get_graph_path(PARAMS['model']), target_size=PARAMS['target_size']
     )
 
 
@@ -38,3 +40,11 @@ def process(inputs, ctx, **kwargs):
         image_output = cv2.imencode(".jpg", image[:, :, ::-1])[1].tostring()
 
     return {'output': image_output}
+
+
+def _parse_resolution(s: str):
+    splitted = s.split('x')
+    if len(splitted) != 2:
+        raise RuntimeError(f'Invalid resolution string: {s}')
+
+    return int(splitted[0]), int(splitted[1])
