43631aab4f2c85d56658b3121bae2b607e18f2b0
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-09-13 11:52:31 +0400

    Optional pose estimation

--- tf_pose/hook.py
+++ tf_pose/hook.py
@@ -14,7 +14,8 @@ o: optic_flow.OpticalFlow = optic_flow.OpticalFlow()
 PARAMS = {
     'model': 'mobilenet_thin',
     'resize_out_ratio': 4.0,
-    'target_size': (432, 368)
+    'target_size': (432, 368),
+    'poses': True,
 }
 load_lock = threading.Lock()
 loaded = False
@@ -24,16 +25,18 @@ def init_hook(**params):
     PARAMS.update(params)
     PARAMS['resize_out_ratio'] = float(PARAMS['resize_out_ratio'])
     PARAMS['target_size'] = _parse_resolution(PARAMS['target_size'])
+    PARAMS['poses'] = helpers.boolean_string(PARAMS['poses'])
     global e
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
 
-    e = estimator.TfPoseEstimator(
-        networks.get_graph_path(PARAMS['model']),
-        target_size=PARAMS['target_size'],
-        tf_config=config,
-    )
+    if PARAMS['poses']:
+        e = estimator.TfPoseEstimator(
+            networks.get_graph_path(PARAMS['model']),
+            target_size=PARAMS['target_size'],
+            tf_config=config,
+        )
 
 
 def process(inputs, ctx, **kwargs):
@@ -46,18 +49,20 @@ def process(inputs, ctx, **kwargs):
                 loaded = True
 
     image, is_video = helpers.load_image(inputs, 'input')
-    humans = e.inference(
-        image,
-        resize_to_default=True,
-        upsample_size=PARAMS['resize_out_ratio']
-    )
+    if PARAMS['poses']:
+        humans = e.inference(
+            image,
+            resize_to_default=True,
+            upsample_size=PARAMS['resize_out_ratio']
+        )
 
     if ctx.drivers[0].driver_name != 'null':
         vectors = o.calc_human_speed(image)
         o.draw_vectors(image, vectors)
         o.draw_boxes(image)
 
-    image = e.draw_humans(image, humans, imgcopy=True)
+    if PARAMS['poses']:
+        image = e.draw_humans(image, humans, imgcopy=True)
 
     if is_video:
         image_output = image
