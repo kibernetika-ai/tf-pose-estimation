14bc73c8144fbe49b2d34e39a4fab13cc9028b6c
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2020-02-14 11:41:47 -0300

    Tag image as latest in build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-image=kuberlab/tfpose:1.0.0
-image_gpu=kuberlab/tfpose:1.0.0-gpu
+image=kuberlab/tfpose:latest
+image_gpu=kuberlab/tfpose:latest-gpu
 
 docker build --tag $image -f Dockerfile.kibernetika .
 docker build --tag $image_gpu -f Dockerfile.kubernetika-gpu .
