27beccd59c377d1260978cea1d2281550e735f4d
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2020-02-14 11:57:37 -0300

    Fix build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ image=kuberlab/tfpose:latest
 image_gpu=kuberlab/tfpose:latest-gpu
 
 docker build --tag $image -f Dockerfile.kibernetika .
-docker build --tag $image_gpu -f Dockerfile.kubernetika-gpu .
+docker build --tag $image_gpu -f Dockerfile.kibernetika-gpu .
 
 if [ "$1" == "--push" ];
 then
